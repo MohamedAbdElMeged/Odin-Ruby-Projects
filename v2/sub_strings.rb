3e9aefc7f335f737a3dbeb398f34c174136068f6
Mohamed Abd El-Meged <mabdelmeged.se@gmail.com>
2023-07-05 22:41:27 +0300

    change substring logic use scan

--- v2/sub_strings.rb
+++ v2/sub_strings.rb
@@ -2,11 +2,10 @@ DICTIONARY = %w[below down go going horn how howdy it i low own part partner sit
 
 def sub_strings(sentence, dictionary)
   result = Hash.new(0)
-  sentence_in_words = sentence.split(' ')
-  sentence_in_words.each do |word|
-    dictionary.each do |dict_word|
-      result[dict_word] += 1 if word.downcase.include? dict_word
-    end
+  downcased_sentence = sentence.downcase
+  dictionary.each do |dict_word|
+    matches = downcased_sentence.scan(dict_word).size
+    result[dict_word] = matches if matches > 0
   end
   result
 end
